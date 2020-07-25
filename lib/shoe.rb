4f19acc834257390fac3a7577335edac35007b6a
Cory Marker <cmarker4@gmail.com>
2020-07-25 12:58:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,7 +7,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     BRANDS.each do |x|
-      if x.include?(@brand)
+      if !(x.include?(@brand))
         BRANDS << @brand
       end
     end
